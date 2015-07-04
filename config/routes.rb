83e2ca735934b0f20c8b65d4c9bdac6df291414f
Jaya Wijono <jayzzwijono@yahoo.com>
2015-07-04 15:23:36 +1000

    set the initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'home#index'
+
+  resources :home, only: [:index, :show]
+  resources :crime_data, only: [:show]
+  resources :quiz, only: [:index]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
